6cc6b59018bf6649c6d9a2e432dba005b95843da
hadrylouis <hadry.louis@gmail.com>
2022-01-29 12:55:32 +0100

    [wip] auditor

--- docker/image-auditor/src/index.js
+++ docker/image-auditor/src/index.js
@@ -1,12 +1,56 @@
 import dgram from "dgram";
 import protocol from "./musician-protocol.js"
+import net from "net"
 
-const s = dgram.createSocket("udp4")
-s.bind(protocol.PROTOCOL_PORT, function() {
+const socket = dgram.createSocket("udp4")
+let musicians = new Map()
+
+socket.bind(protocol.PROTOCOL_PORT, () => {
     console.log("Joining multicast group")
-    s.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS)
+    socket.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS)
 })
 
-s.on('message', function(msg, source) {
-	console.log("Data has arrived: " + msg + ". Source port: " + source.port);
+socket.on('message', function(msg, source) {
+    const {uuid, sound} = JSON.parse(msg)
+
+    let instrument = protocol.INSTRUMENTS.get(sound)
+    let date = Date.now()
+
+    if(!musicians.has(uuid)) {
+        musicians.set(uuid, {uuid: uuid, instrument: instrument, activeSince: date, lastActive: date})
+    } else {
+        musicians.set(uuid, {uuid: uuid, instrument: instrument, activeSince: musicians.get(uuid).activeSince, lastActive: date})
+    }
+
+    console.log("Data has arrived " + msg + " Source port: " + source.port)
+});
+
+const server = net.createServer()
+const PORT = 2205
+
+server.listen(PORT, () => {
+    console.log("TCP server is running on port " + PORT)
+}).on('connection', (conn) => {
+    console.log("CONNECTED: " + conn.remoteAddress + ":" + conn.remotePort)
+    
+    const now = Date.now()
+    const res = Array.from(musicians.entries()).filter(([uuid, musician]) => {
+        console.log("DELTA : ")
+        console.log(now - musician.lastActive)
+        let removed = now - musician.lastActive > protocol.PROTOCOL_TIMEOUT
+        if(removed) {
+            musicians.delete(uuid)
+        }
+        return !removed
+    }).map(([uuid, musician]) => ({
+        uuid,
+        instrument: musician.instrument,
+        activeSince: new Date(musician.activeSince) 
+    }))
+   
+    conn.write(JSON.stringify(res))
+    
+    conn.end()
+}).on('error', (conn) => {
+    console.log;
 });
--- docker/image-auditor/src/musician-protocol.js
+++ docker/image-auditor/src/musician-protocol.js
@@ -1,4 +1,14 @@
+const entries = [
+    ["piano", "ti-ta-ti"],
+    ["trumpet", "pouet"],
+    ["flute", "trulu"],
+    ["violin", "gzi-gzi"],
+    ["drum", "boum-boum"]
+]
+
 export default {
     PROTOCOL_MULTICAST_ADDRESS: "239.255.22.5",
-    PROTOCOL_PORT: 9907
+    PROTOCOL_PORT: 9907,
+    PROTOCOL_TIMEOUT: 5000,
+    INSTRUMENTS: new Map([...entries].map(e => e.reverse()))
 }
